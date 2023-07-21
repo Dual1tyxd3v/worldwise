42a3cbe14ae33d64b9a2a5e666a3891859aadd4c
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-21 17:15:35 +0700

    Added markers rendering

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -7,21 +7,19 @@ import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import { Icon, PointExpression } from 'leaflet';
 
 import { LatLngExpression } from 'leaflet';
+import { useOwnContext } from '../../contexts/cities-context';
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [position, setPosition] = useState<LatLngExpression>([40, 0]);
+  const { cities } = useOwnContext();
   const navigate = useNavigate();
+
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate(APP_ROUTE.FORM);
-      }}
-    >
+    <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
         center={position}
@@ -32,20 +30,23 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker
-          position={position}
-          icon={
-            new Icon({
-              iconUrl: markerIconPng,
-              iconSize: [22, 32],
-              iconAnchor: position as PointExpression,
-            })
-          }
-        >
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            key={`map_marker_${city.id}`}
+            position={[city.position.lat, city.position.lng]}
+            icon={
+              new Icon({
+                iconUrl: markerIconPng,
+                iconSize: [22, 32],
+                iconAnchor: position as PointExpression,
+              })
+            }
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
