563382b3e518c862d3e00b8dfa7f73adebc3fffc
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-21 17:06:21 +0700

    Added leaflet map

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,15 +1,52 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './map.module.css';
 import { APP_ROUTE } from '../../const';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
+import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import { Icon, PointExpression } from 'leaflet';
+
+import { LatLngExpression } from 'leaflet';
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [position, setPosition] = useState<LatLngExpression>([40, 0]);
   const navigate = useNavigate();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
+
   return (
-    <div className={styles.mapContainer} onClick={() => {navigate(APP_ROUTE.FORM)}}>
-      {lat} / {lng}
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate(APP_ROUTE.FORM);
+      }}
+    >
+      <MapContainer
+        className={styles.map}
+        center={position}
+        zoom={11}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={position}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [22, 32],
+              iconAnchor: position as PointExpression,
+            })
+          }
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
